9f9a81fbbc7d2aefa2d33146fc3ee9e3a3ba02d2
Stanislav Latsko <01latsko10@gmail.com>
2024-03-19 19:49:23 +0100

    Initial commit

---
+++ src/main/java/latsko/sms/DemoForStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package latsko.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForStudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForStudentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/latsko/sms/DemoForStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package latsko.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForStudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
